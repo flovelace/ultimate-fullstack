f52e86d714b202a6e007cd996bb8a2a398cf5e9e
flovelace <lovelace.af@gmail.com>
2022-12-18 16:52:52 -0500

    added types of console methods. Added first variable data type

---
+++ topic-4-javascript/lesson-1/script.js
@@ -0,0 +1,15 @@
+// An Introduction to JavaScript Syntax
+// Your first function: the console log
+// Console is the function then the period is like accessing a sub folder
+console.log('You can write anything here to the console!');
+// Another function the console has:
+console.warn('Uh oh! I am a warning!');
+// And another...
+console.error('Error! Error! I am an error!');
+
+// Storing data in variables. Think of a variable like a box that can contain a specific type of data
+// NOTE! var is bad - don't use it. This is for demonstrative purposes only, and it's important to know it exists.
+// Inside this variable is a string
+var yourName = 'Franz Kafka';
+// Log it to the console
+console.log(yourName);
