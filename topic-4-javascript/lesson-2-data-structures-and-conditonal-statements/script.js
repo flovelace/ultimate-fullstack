f89c3eb6b9df46b4bd871c3e8592b842d9d02483
flovelace <lovelace.af@gmail.com>
2022-12-21 14:52:26 -0500

    added conditional examples

--- topic-4-javascript/lesson-2-data-structures-and-conditonal-statements/script.js
+++ topic-4-javascript/lesson-2-data-structures-and-conditonal-statements/script.js
@@ -15,3 +15,28 @@ const obj = {
   occupation: 'knight',
   age: 23,
 }; // objects hold key: value. Can be accessed with obj['name']
+
+// Conditional Statements
+// Conditional Statements execute code based on whether a statement is true, execute the code within the curly braces
+// Example using string data
+const person = 'Katherine';
+
+if (person === 'Katherine') {
+  console.log('Meet Katherine. She is the hero of the story!');
+} else {
+  // if the above statement is false, execute this code
+  if (person != 'Katherine') console.log('Meet ' + person + '.');
+}
+
+// Another example of a conditional statement using number data
+const age = 60;
+
+if (age < 18) {
+  console.log('You are a child.');
+}
+if (age >= 18) {
+  console.log('You are legally an adult - congrats!');
+}
+if (age >= 40) {
+  console.log('You are doing great!');
+}
