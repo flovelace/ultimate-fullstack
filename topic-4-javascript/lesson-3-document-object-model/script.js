cb51de0c1f4923e9145de9eb481c54310a2dc121
flovelace <lovelace.af@gmail.com>
2022-12-28 11:52:10 -0500

    added get element by ID

--- topic-4-javascript/lesson-3-document-object-model/script.js
+++ topic-4-javascript/lesson-3-document-object-model/script.js
@@ -1,1 +1,6 @@
 // Document Object Model (DOM)
+const box = document.getElementById('box');
+box.innerText = 'This text has been changed on the DOM.';
+
+const redBox = document.getElementById('redBox');
+redBox.innerHTML = '<strong>This text has been bolded on the DOM</strong>';
