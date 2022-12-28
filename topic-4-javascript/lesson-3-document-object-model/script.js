26ec5c73ace1ce293d56f1707574c47ae46c2bff
flovelace <lovelace.af@gmail.com>
2022-12-28 12:02:47 -0500

    added object and accessing object notes

--- topic-4-javascript/lesson-3-document-object-model/script.js
+++ topic-4-javascript/lesson-3-document-object-model/script.js
@@ -1,6 +1,21 @@
 // Document Object Model (DOM)
+// Select an element by id
+// create a variable to contain the element
 const box = document.getElementById('box');
 box.innerText = 'This text has been changed on the DOM.';
 
 const redBox = document.getElementById('redBox');
 redBox.innerHTML = '<strong>This text has been bolded on the DOM</strong>';
+
+// JavaScript objects
+// It is common to declare objects with const
+const person = {
+  name: 'Chris',
+  age: 23,
+  occupation: 'Knight',
+  favFoods: ['biscuits', 'venison', 'rice pudding'],
+};
+
+// How to access the values in an object
+console.log(person.age);
+console.log(person['favFoods']);
