a6a2a567b43fd023d6ead6f4835f317d6d487775
flovelace <lovelace.af@gmail.com>
2022-12-30 13:12:40 -0500

    added for and forEach method examples

--- topic-4-javascript/lesson-5-arrays/script.js
+++ topic-4-javascript/lesson-5-arrays/script.js
@@ -16,3 +16,13 @@ console.log('apple');
 // Add elements to the beginning of an array
 fruitBasket.unshift('peach', 'plum', 'pear');
 console.log(fruitBasket);
+
+// The for loop is boolean. The code within the curly braces will execute until the index is no longer less than the length of the array. When the condition is false, it will exit out of the loop.
+for (let i = 0; i < fruitBasket.length; i++) {
+  console.log(i, fruitBasket[i]);
+}
+
+// forEach method (loops through each item the array)
+fruitBasket.forEach((fruit) => {
+  console.log(fruit);
+});
