2fcae0d8d92c5433b43e51cb8e0adbfdd2b0a659
flovelace <lovelace.af@gmail.com>
2023-01-04 21:49:16 -0500

    added a basic addition function

---
+++ topic-4-javascript/lesson-7-functions-reusing-code/script.js
@@ -0,0 +1,10 @@
+// To create a function, use the function keyword followed by parenthesis.
+// Inside the parenthesis, include parameter names separated by a comma
+// Inside the curly braces, use the return keyword
+function sum(x, y) {
+  return x + y;
+}
+
+let total = sum(4, 4);
+
+console.log(total);
