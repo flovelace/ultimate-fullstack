3ebd748f88600af53bc4c9d482760a878a720a90
flovelace <lovelace.af@gmail.com>
2023-01-05 21:06:11 -0500

    added number casting example

--- topic-4-javascript/lesson-7-functions-reusing-code/script.js
+++ topic-4-javascript/lesson-7-functions-reusing-code/script.js
@@ -2,7 +2,7 @@
 // Inside the parenthesis, include parameter names separated by a comma
 // Inside the curly braces, use the return keyword.
 
-// A simple addition function
+// A simple addition function using number type
 function sum(x, y) {
   return x + y;
 }
@@ -10,6 +10,16 @@ function sum(x, y) {
 let total = sum(4, 4);
 
 console.log(total);
+
+// A simple function using data type casting
+function multiplyNumber(num1, num2) {
+  // added Number ensures correct datatype is used
+  const total = Number(num1) * Number(num2);
+  return total;
+}
+
+const multiplyNumberTotal = multiplyNumber(2, 3);
+console.log(multiplyNumberTotal);
 
 // A simple string function
 function sayHi(name) {
