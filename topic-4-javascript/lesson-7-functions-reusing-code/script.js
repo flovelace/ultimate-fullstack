3155aa7c23f810ea357a205f494c8cef2b7f9ce1
flovelace <lovelace.af@gmail.com>
2023-01-05 20:58:25 -0500

    added string example

--- topic-4-javascript/lesson-7-functions-reusing-code/script.js
+++ topic-4-javascript/lesson-7-functions-reusing-code/script.js
@@ -1,6 +1,8 @@
 // To create a function, use the function keyword followed by parenthesis.
 // Inside the parenthesis, include parameter names separated by a comma
-// Inside the curly braces, use the return keyword
+// Inside the curly braces, use the return keyword.
+
+// A simple addition function
 function sum(x, y) {
   return x + y;
 }
@@ -8,3 +10,11 @@ function sum(x, y) {
 let total = sum(4, 4);
 
 console.log(total);
+
+// A simple string function
+function sayHi(name) {
+  return `Hello there, ${name}!`;
+}
+
+let yourName = sayHi('Ada');
+console.log(yourName);
