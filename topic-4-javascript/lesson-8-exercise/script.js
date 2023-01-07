8ecf4216c7704d4c8e42f14ee7ddc00cf9ad7369
flovelace <lovelace.af@gmail.com>
2023-01-06 21:18:59 -0500

    added answers to exercise

---
+++ topic-4-javascript/lesson-8-exercise/script.js
@@ -0,0 +1,26 @@
+// Step 1: Fill in these three consts by selecting the right nodes by their HTML ID's
+const name = document.getElementById('name');
+name.innerText = 'Chris';
+const age = document.getElementById('age');
+age.innerText = 23;
+const dogYears = document.getElementById('dogyears'); // Fill this in.
+
+// Step 2. Set the values.
+// Example: myNode.innerText = "something"
+// Set `name` and `age` in here
+
+function showDogYears(age) {
+  // Step 3. Cast `age` as a number
+  dogYears.innerText = Number;
+  age * 7;
+  return age * 7;
+}
+
+// Step 4. Set the parameter of showDogyears()
+dogYears.innerText = showDogYears(23);
+
+// Step 5. Select all the .fake-input classes and give them the class of .real-input. This may take a little "googling" to find the solution, but that's half of what web development is all about.
+const fakeInputs = document.querySelectorAll('.fake-input');
+fakeInputs.forEach((node) => {
+  node.classList.add('real-input');
+});
